44febf742ff994c03cea05537371900b2d2fd5af
Savan120 <patelsavan931406891@gmail.com>
2024-12-16 11:58:51 +0530

    Add custom field and doctypes

---
+++ solufy/solufy/doctype/language_type/language_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, savan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Language Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ solufy/solufy/doctype/language_type/test_language_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, savan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLanguageType(FrappeTestCase):
+	pass
